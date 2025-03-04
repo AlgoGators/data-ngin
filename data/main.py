29ac23d7e1a3a2251032ab29a21609340cfa9d48
ben <benadelman2006@gmail.com>
2025-03-04 17:37:34 -0500

    testing multiple datasources

--- data/main.py
+++ data/main.py
@@ -2,6 +2,7 @@ import logging
 import asyncio
 from utils.dynamic_loader import load_config
 from data.orchestrator import Orchestrator
+import os
 
 
 def main() -> None:
@@ -18,20 +19,22 @@ def main() -> None:
 
     try:
         # Load configuration
-        # config_path = "data/config/config.yaml"  # Path to the YAML configuration file locally
-        config_path = "/app/data/config/config.yaml"  # Path to the YAML configuration file in the Docker container
-        logging.info(f"Loading configuration from {config_path}")
-        config = load_config(config_path)
+        file_path = "data/config"
+        for filename in os.listdir(file_path):
+            file_path = os.path.join(file_path, filename)
+            config_path = "data/config/config.yaml"  # Path to the YAML configuration file in the Docker container
+            logging.info(f"Loading configuration from {config_path}")
+            config = load_config(config_path)
 
-        # Initialize the Orchestrator
-        logging.info("Initializing orchestrator...")
-        orchestrator = Orchestrator(config=config)
+            # Initialize the Orchestrator
+            logging.info("Initializing orchestrator...")
+            orchestrator = Orchestrator(config=config)
 
-        # Run the pipeline
-        logging.info("Starting the data pipeline...")
-        asyncio.run(orchestrator.run())
+            # Run the pipeline
+            logging.info("Starting the data pipeline...")
+            asyncio.run(orchestrator.run())
 
-        logging.info("Pipeline execution completed successfully.")
+            logging.info("Pipeline execution completed successfully.")
 
     except Exception as e:
         logging.error(f"Pipeline execution failed: {e}")
--- utils/dynamic_loader.py
+++ utils/dynamic_loader.py
@@ -22,7 +22,6 @@ def load_config(config_path: str = "data/config/config.yaml") -> Dict[str, Any]:
     """
     if not os.path.exists(config_path):
         raise FileNotFoundError(f"Configuration file not found at {config_path}")
-
     with open(config_path, "r") as file:
         try:
             config: Dict[str, Any] = yaml.safe_load(file)
